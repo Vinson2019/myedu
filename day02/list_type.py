fb14cdb7e4d48010a63a6b3a07ff31075f6f30bc
赵文生 <20930385@qq.com>
2019-04-19 11:59:03 +0800

    新增 查询

---
+++ day02/list_type.py
@@ -0,0 +1,13 @@
+def lest_name(a):
+    print(a[2])
+    print(a[1:3])
+def lest_qname():
+    alist = [1,'世','界','你','好']
+    print(alist[0:6])
+    alist[0] = 5
+
+
+if __name__ == '__main__':
+   alist = ['china','日本',825,961,]
+   lest_name(alist)
+   lest_qname()
